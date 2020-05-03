fe70fa92e0fee9fd1eca195e106bcee27d5893c3
Sahstranshu Dubey <sahstranshu.dubey@gmail.com>
2020-05-03 20:54:52 +0530

    Question pojo, seviceInterface,service,Dao,and addQuestion,find all question and deleteQuestion  metodsadded

---
+++ OnlineTestManagement/src/main/java/com/capgemin/service/QuestionServiceI.java
@@ -0,0 +1,13 @@
+package com.capgemin.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.capgemini.entity.Question;
+
+public interface QuestionServiceI {
+public String addQuestion(int testId,Question question);
+public String deleteQuestion(int questionId);
+public List<Question> viewAll();
+public Optional<Question> findById(int id);
+}
